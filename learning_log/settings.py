d3dec7f1e4e9ea19f46088c1b544d2ad0351b718
chisiliangmi <2993436102@qq.com>
2022-05-25 01:19:28 +0800

    404、500错误界面修改

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from re import template
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,4 +142,10 @@ BOOTSTRAP3 = {
 
 # Heroku设置
 import django_heroku
+
 django_heroku.settings(locals())
+
+""" if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG == True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG == False """
