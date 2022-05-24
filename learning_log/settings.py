a9ba5fbfe6b1b176a1762adc75ccc83ed32d09bc
chisiliangmi <2993436102@qq.com>
2022-05-24 23:43:24 +0800

    修改settings.py中DEBUG=False

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b%t^iuci6w6be+p6g5gx@&$18n*vqcdjw*ed)$2v_x@b^2tri6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
